df6c54d65fbe1dfcfc09eb08c358b2ae75ae433b
Moussassoss <ssossinfo88@gmail.com>
2025-11-05 16:02:21 +0200

    separator import line in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Library, Book
 
 # Library + Books
