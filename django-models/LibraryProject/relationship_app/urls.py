dc4cddbf50555e48dcbbab8c2d001bda67ca74ac
Moussassoss <ssossinfo88@gmail.com>
2025-11-05 16:12:50 +0200

    edited urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,21 +1,20 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from . import views  # import the whole views module
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    
-    path('admin-dashboard/', admin_view, name='admin_view'),
-    path('librarian-dashboard/', librarian_view, name='librarian_view'),
-    path('member-dashboard/', member_view, name='member_view'),
 
-    path('add-book/', add_book, name='add_book'),
-    path('edit-book/<int:book_id>/', edit_book, name='edit_book'),
-    path('delete-book/<int:book_id>/', delete_book, name='delete_book'),
+    path('admin-dashboard/', views.admin_view, name='admin_view'),
+    path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
+    path('member-dashboard/', views.member_view, name='member_view'),
+
+    path('add-book/', views.add_book, name='add_book'),
+    path('edit-book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete-book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
