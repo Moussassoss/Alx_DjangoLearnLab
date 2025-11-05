0fb1283603dff894488ef3708c9f709c98c5bb6e
Moussassoss <ssossinfo88@gmail.com>
2025-11-05 15:02:00 +0200

    corrected views and settings to include the new app and its real paths.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
-# Function-based view for listing all books
+# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # required by checker
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 # Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    templates = "relationship_app/library_detail.html"
+    context_object_name = "library"
