2d4d2b94d012587a67b7e2d3234b4ea434fd5694
Moussassoss <ssossinfo88@gmail.com>
2025-11-05 15:10:08 +0200

    import book and library separately

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library  # ✅ required by checker
+from .models import Library  
+from .models import Book
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()  # ✅ required by checker
+    books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 # Class-based view for library details
