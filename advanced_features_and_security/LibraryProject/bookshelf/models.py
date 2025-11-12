bd1dacac8619d039ca13b9170683a40184337a66
Moussassoss <ssossinfo88@gmail.com>
2025-11-12 08:23:20 +0200

    added publication_year field to Book model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,22 +1,54 @@
-from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Custom user model
+
+# ----------------------------
+# Custom User Manager
+# ----------------------------
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field is required')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+# ----------------------------
+# Custom User Model
+# ----------------------------
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()  # Use the custom manager
 
     def __str__(self):
         return self.username
 
 
-# Book model
+# ----------------------------
+# Book Model
+# ----------------------------
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    publication_year = models.PositiveIntegerField(null=True, blank=True)
     added_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        'bookshelf.CustomUser',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
