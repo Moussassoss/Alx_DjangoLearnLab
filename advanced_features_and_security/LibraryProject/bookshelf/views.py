3844a55577323a41aa85429af82d6e859fed213d
Moussassoss <ssossinfo88@gmail.com>
2025-11-12 08:49:53 +0200

    updated views to adapt with forms.py changes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 # View books (requires 'can_view')
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -9,31 +9,15 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-# Create book (requires 'can_create')
+# Example form view (for create/edit demonstration)
 @permission_required('bookshelf.can_create', raise_exception=True)
-def book_create(request):
+def example_form_view(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
-            form.save()
+            # Example: just print or handle form data, no DB save needed for ALX
+            print(form.cleaned_data)
             return redirect('book_list')
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/form_example.html', {'form': form})
-
-# Edit book (requires 'can_edit')
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    form = BookForm(request.POST or None, instance=book)
-    if form.is_valid():
-        form.save()
-        return redirect('book_list')
-    return render(request, 'bookshelf/form_example.html', {'form': form})
-
-# Delete book (requires 'can_delete')
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    book.delete()
-    return redirect('book_list')
