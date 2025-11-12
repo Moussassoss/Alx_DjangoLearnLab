49e7236d6a18d6efce52f687ceefa78cd52cb25b
Moussassoss <ssossinfo88@gmail.com>
2025-11-12 08:14:21 +0200

    updated bookshelf models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,12 +1,27 @@
-# bookshelf/models.py
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+# Custom user model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+# Book model
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='books_added')
-    # other fields...
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='books_added'
+    )
 
     class Meta:
         permissions = [
