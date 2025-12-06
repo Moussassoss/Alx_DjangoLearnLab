a230bbc68fa2e732f20356e5bd22b6bae9909522
Moussassoss <ssossinfo88@gmail.com>
2025-12-06 17:26:52 +0200

    changed from comments to comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from .views import (
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
-    add_comment, edit_comment, delete_comment, RegisterView, ProfileView
+    CommentCreateView, CommentUpdateView, CommentDeleteView,
+    RegisterView, ProfileView
 )
 from django.contrib.auth import views as auth_views
 
@@ -17,9 +18,9 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 
     # comments
-    path('post/<int:post_pk>/comments/add/', add_comment, name='add_comment'),
-    path('comments/<int:comment_pk>/edit/', edit_comment, name='edit_comment'),
-    path('comments/<int:comment_pk>/delete/', delete_comment, name='delete_comment'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
 
     # auth
     path('register/', RegisterView.as_view(), name='register'),
