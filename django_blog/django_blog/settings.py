6d11874e12558752b3e1f15c64e532b71b89f562
Moussassoss <ssossinfo88@gmail.com>
2025-12-06 17:17:13 +0200

    changed the settings.py file to update the database configuration.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,15 +75,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 
 # Password validation
