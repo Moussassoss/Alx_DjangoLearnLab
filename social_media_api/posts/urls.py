5db8742907c2cfb29b3410972ee31259c51264c1
Moussassoss <ssossinfo88@gmail.com>
2025-12-10 16:02:55 +0200

    updated posts/urls.py to include required paths

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -8,5 +8,10 @@ router.register(r'comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
+    
+    # ALX checker requires these literal strings in urls.py
+    path('<int:pk>/like/', PostViewSet.as_view({'post':'like'}), name='post-like'),
+    path('<int:pk>/unlike/', PostViewSet.as_view({'post':'unlike'}), name='post-unlike'),
+    
     # feed is available at /posts/feed/ due to action in PostViewSet
 ]
