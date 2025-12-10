39bc2843d1294b85c1c7f142de665ddb34dc7ec2
Moussassoss <ssossinfo88@gmail.com>
2025-12-10 16:12:15 +0200

    added static roots

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -27,6 +27,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
 
+
+STATIC_ROOT = "staticfiles"
+
 # Security settings
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
